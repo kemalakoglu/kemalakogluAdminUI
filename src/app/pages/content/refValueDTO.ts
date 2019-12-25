7ff1be04c2fbae90aa9058663cb8feb39e65f161
Kemal Akoglu <kemal.akoglu@doco.com.tr>
2019-12-25 12:59:11 +0300

    RefValue operations were implemented

--- src/app/pages/content/refValueDTO.ts
+++ src/app/pages/content/refValueDTO.ts
@@ -1,12 +1,11 @@
 import {RefTypeDTO} from "../page/refTypeDTO";
 
-export class RefValueDTO {
+export class RefValueDTO{
   public Id: number;
   public Value: string;
   public RefType: RefTypeDTO;
   public Name: string;
   public Status: boolean;
-  public InsertDate: string;
-  public UpdateDate: string;
   public IsActive: boolean;
 }
+
